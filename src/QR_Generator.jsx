5ab86ae018071eafb89c9442c52506a14364c4f3
ATHARV PATIL <atharvpatil5177@gmail.com>
2023-08-08 11:02:18 +0530

    added styles and fixed some bugs

--- src/QR_Generator.jsx
+++ src/QR_Generator.jsx
@@ -1,5 +1,6 @@
 import QRCode from "react-qr-code";
 import { useState } from "react";
+import styled from "styled-components";
 
 const QR_Generator = () => {
   const [link, setLink] = useState("");
@@ -10,14 +11,39 @@ const QR_Generator = () => {
 
   return (
     <>
-      <h1>Enter the link whose QR code has to be generated</h1>
-      <input onChange={handleLink}></input>
-      <button onClick={() => setHasButtonClicked(!hasButtonClicked)}>
-        Generate QR
-      </button>
-      {hasButtonClicked && <QRCode value={link} />}
+      <Heading className="heading">
+        <h1>Enter the link whose QR code has to be generated</h1>
+        <input onChange={handleLink}></input>
+      </Heading>
+      <ButtonContainer className="Qr">
+        <button onClick={() => setHasButtonClicked(true)}>Generate QR</button>
+      </ButtonContainer>
+      <div></div>
+      <div>{hasButtonClicked && <QRCode value={link} />}</div>
     </>
   );
 };
+
+const Heading = styled.div`
+  h1 {
+    margin-top: 2rem;
+  }
+  text-align: center;
+  input {
+    border: none;
+    background-color: white;
+    color: black;
+    border-radius: 3px;
+    padding: 5px;
+    margin-top: 2rem;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  button {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+`;
 
 export default QR_Generator;
